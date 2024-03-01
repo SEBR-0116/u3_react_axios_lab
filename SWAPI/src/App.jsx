5f27285b825008edf03c735a4cebc34c25deeaf6
MiaDHayes <miadeyon@outlook.com>
2024-03-01 13:59:15 -0600

    data populated

--- SWAPI/src/App.jsx
+++ SWAPI/src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BASE_URL } from './globals'
 import { Route, Routes } from 'react-router-dom'
+import axios from 'axios'
 import './App.css'
 import Header from './components/Header'
 import StarshipList from './components/StarshipList'
 import Home from './components/Home'
 
 function App() {
+  const [starships, setStarships] = useState([])
+  const [selectedStarship, setSelectedStarship] = useState(null)
+
+
+  useEffect(() => {
+    const getStarships = async () => {
+      const response = await axios.get(`${BASE_URL}`)
+      setStarships(response.data.results)
+      console.log(response)
+    } 
+
+    getStarships()
+  }, [])
+
+  console.log(starships)
+
 
   return (
     <div className='App'>
@@ -13,7 +31,7 @@ function App() {
         
         <Routes>
           <Route exact path='/' element= {<Home />} />
-          <Route exact path='/starships' element= {<StarshipList />}/>
+          <Route exact path='/starships' element= {<StarshipList starships={starships}/>}/>
         </Routes>
     </div>
   )
--- SWAPI/src/components/StarshipList.jsx
+++ SWAPI/src/components/StarshipList.jsx
@@ -1,7 +1,29 @@
-const StarshipList = () => {
-    return (
-        <h2>Starship stuffs</h2>
+
+
+const StarshipList = (props) => {
+    console.log(props.starships)
+
+    if (!props.starships) {
+        return <h3>Loading puhleeze wait</h3>
+    } else {
+
+        return (
+            <div className="starship-list">
+                {
+                    props.starships.map((starship) => (
+                        <div className="ship" key={starship.id}>
+                            <h3>{starship.name}</h3>
+                            <h3>{starship.model}</h3>
+                            <h3>{starship.manufacturer}</h3>
+                            <h3>{starship.cost_in_credits}</h3>
+                            <button>View Starship</button>
+                        </div>
+                    ))
+                }
+            </div>
     )
+}
+
 }
 
 export default StarshipList
