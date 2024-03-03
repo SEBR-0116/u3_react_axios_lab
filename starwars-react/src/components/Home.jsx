8fe2fc88a5d1b572a95456bdde90fae869f0b4fa
larsonscott89 <scottlarson0305@gmail.com>
2024-03-03 17:57:24 -0500

    Done

--- starwars-react/src/components/Home.jsx
+++ starwars-react/src/components/Home.jsx
@@ -1,21 +1,13 @@
-import React, { useEffect, useRef } from "react"
+import ReactPlayer from 'react-player'
 import './home.css'
 
 export default function Home () {
 
-  const videoEl = useRef(null);
-
-  const attemptPlay = () => {
-    videoEl &&
-      videoEl.current &&
-      videoEl.current.play().catch(error => {
-        console.error("Error attempting to play", error)
-      })
-  }
   return (
   <div className='home-container'>
-    <div className='video-player'>
-    <ReactPlayer url={'https://www.youtube.com/watch?v=7jK-jZo6xjY'} autoPlay controls={true} />
+    <h1 className='title'> Let the Force be with you </h1>
+    <div className=''>
+    <ReactPlayer url={'https://vimeo.com/414748279'} className='video-player' controls autoPlay muted/>
     </div>
   </div>
   )
