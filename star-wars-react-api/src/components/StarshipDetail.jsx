f6186e0bef6dd0ab323688aae53ccabf30b48c72
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-03-05 08:30:02 -0500

    commented out code to troubleshoot

--- star-wars-react-api/src/components/StarshipDetail.jsx
+++ star-wars-react-api/src/components/StarshipDetail.jsx
@@ -1,30 +1,31 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import StarshipList from './StarshipList'//is something like this needed to pass the props since not defined at route in main?
+import axios from 'axios'
 
 const StarshipDetail = (props) => {
     //is there props that get passed down here?
-    console.log(starships)
+    console.log(props)
 
     const [starship, setStarship] = useState('')
 
-    let { id } = useParams()
-    useEffect(() => {
-    let selectedStarship = props.starships.find((starship) => starship.id===parseInt(id))
-
-    setStarship(selectedStarship)
-    }, [props.starships, id])
-
+    // let { id } = useParams()
     // useEffect(() => {
-    //     let selectedStarship = starships.find((starship) => starship.id===parseInt(id))
-    
-    //     setStarship(selectedStarship)
-    //     }, [starships, id])
+    // let selectedStarship = props.starships.find((starship) => starship.id===parseInt(id))
 
-    return starship ? (
-        <div key={starship.id}>
-            <h1>{starship.name}</h1>
-        </div>
-    ) : <h1>Starship not found</h1>; //could also be null
+    // setStarship(selectedStarship)
+    // }, [props.starships, id])
+
+    // // useEffect(() => {
+    // //     let selectedStarship = starships.find((starship) => starship.id===parseInt(id))
+    
+    // //     setStarship(selectedStarship)
+    // //     }, [starships, id])
+
+    // return starship ? (
+    //     <div key={starship.id}>
+    //         <h1>{starship.name}</h1>
+    //     </div>
+    // ) : <h1>Starship not found</h1>; //could also be null
 }
 export default StarshipDetail
