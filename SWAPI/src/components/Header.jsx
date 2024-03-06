a150b2bfe2d731bd2c14329cfc670fd019859833
MiaDHayes <miadeyon@outlook.com>
2024-03-06 17:04:06 -0600

    css

--- SWAPI/src/components/Header.jsx
+++ SWAPI/src/components/Header.jsx
@@ -3,7 +3,7 @@ import Nav from "./Nav"
 export default function Header () {
     return (
         <div className="header">
-            <h1>Starships</h1>
+            <h5></h5>
             <Nav />
         </div>
     )
